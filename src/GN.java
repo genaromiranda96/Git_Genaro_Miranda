50a173a83799df70bce6d6ce88a0522895967f03
“Genaro <ramoon1996@hotmail.com>
2023-01-22 22:37:45 +0100

    Modificación 3

--- src/GN.java
+++ src/GN.java
@@ -2,7 +2,7 @@
 public class GN {
 	public static void main(String[] args) {
 		System.out.println("Alumno: Ramon Genaro Miranda");
-		System.out.println("Modificacion 1");
+		System.out.println("Modificacion desde Master - Cambiado");
 
 	}
 }
